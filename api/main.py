97ce80cb36e9dcecda08d9d228cb753a6f7071b1
Muhammad Arslan <marslanmustafa391@gmail.com>
2026-05-03 20:05:23 +0500

    cred false

--- api/main.py
+++ api/main.py
@@ -45,7 +45,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "https://rps.marslanmustafa.com", "http://localhost:3001"],
-    allow_credentials=True,
+    # allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
