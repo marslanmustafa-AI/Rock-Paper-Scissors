46b50526867296011a237441735ad2028a6e20f5
Muhammad Arslan <marslanmustafa391@gmail.com>
2026-05-03 20:01:23 +0500

    cors added

--- api/main.py
+++ api/main.py
@@ -44,7 +44,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000", "https://rps.marslanmustafa.com", "http://localhost:3001"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
